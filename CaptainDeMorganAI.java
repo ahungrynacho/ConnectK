7432dbb0759ce89996eeb765d961c0c73b9f05d9
ahungrynacho <brianbhuynh@gmail.com>
2016-11-19 17:10:53 -0800

    Refactored

--- CaptainDeMorganAI.java
+++ CaptainDeMorganAI.java
@@ -2,51 +2,47 @@ import connectK.CKPlayer;
 import connectK.BoardModel;
 import java.awt.Point;
 import java.util.Random;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class CaptainDeMorganAI extends CKPlayer {
-	private int cutOff = 5;
-	private int alpha = Integer.MIN_VALUE; // eventually becomes the largest value along Max's path
-	private int beta = Integer.MAX_VALUE; // eventually becomes the smallest value along Min's path
-	private int maxNodes = Integer.MAX_VALUE;
-	private Scanner input = new Scanner(System.in);
-	private ArrayList<Point> edgeCases = new ArrayList<Point>();
+	private int cutOff;
+	private int alpha;
+	private int beta;
 	
 	// CaptainDeMorgainAI's arg constructor calls CKPlayer's arg constructor
 	public CaptainDeMorganAI(byte player, BoardModel state) {
 		super(player, state);
 		teamName = "CaptainDeMorganAI";
+		cutOff = setCutoff(state.gravityEnabled());
+		alpha = Integer.MIN_VALUE; // eventually becomes the largest value along Max's path
+		beta = Integer.MAX_VALUE; // eventually becomes the smallest value along Min's path
 	}
 	
 	@Override
 	public Point getMove(BoardModel state) {
-		
-		Point move = null;
-//		for (int d = 3; d < 5; d++) {
-			long start = System.currentTimeMillis();
-			Node bestNode = this.minimax(new Node(state, 0, null, this.cutOff), this.alpha, this.beta);
-//			if (System.currentTimeMillis() - start > 8000) {
-				move = bestNode.getMove();
-//				break;
-//			}
-//		}
-		
+		Node n;
+		Point move;
+		if (state.getLastMove() != null) {
+			n = new Node(state, 0, null, cutOff); // initialized to null if AI is player 1 making the first move
+			move = minimax(n, alpha, beta).getMove(); 
+		}
+		else { // CaptainDeMorgain is the first to move
+			move = new Point(state.getWidth() / 2, state.getHeight() / 2);
+		}
 		
 		int nodes = Node.getNodeCount();
 		System.out.println("Total Nodes Computed:" + nodes);
 		Node.resetNodeCount();
 		
-		// might need to adjust the heuristic
-		if (nodes > maxNodes) {
-			edgeCases.add(move);
-			for (Point p : edgeCases) {
-				System.out.println("Node.getNodeCount() > maxNodes: " + p);
-			}
-		}
-		maxNodes = nodes;
+//		if (nodes > maxNodes) {
+//			System.out.println("Node.getNodeCount() > maxNodes: " + move);
+//			input.nextLine();
+//		}
+//		else {
+//			maxNodes = nodes;
+//			Node.resetNodeCount();
+//		}
 		
 		return move;
 	}
@@ -56,11 +52,18 @@ public class CaptainDeMorganAI extends CKPlayer {
 		return getMove(state);
 	}
 	
+	private int setCutoff(boolean gravity) {
+		if (gravity)
+			return 5;
+		else
+			return 4;
+	}
+	
 	private Node minimax(Node n, int alpha, int beta) {
 //		gravity on: branching factor = width
 //		gravity off: branching factor = width * height
 
-		HashSet<Point> nextMoves = n.nextMoves(n.getState().gravityEnabled());
+		HashSet<Point> nextMoves = n.getCacheNextMoves();
 //		System.out.println("Number of Next Moves: " + nextMoves.size());
 		
 		int bestRank = Integer.MIN_VALUE;
@@ -69,17 +72,16 @@ public class CaptainDeMorganAI extends CKPlayer {
 		for (Point p : nextMoves) {
 			Node nextNode = new Node(n.nextState(p), n.getDepth()+1, p, n.getCutoff()); // currently at depth = 1; lastMove() returns 2
 			Node.incNodeCount();
-			int rank = this.min(nextNode, nextNode.getDepth(), alpha, beta).getRank();
+			int rank = min(nextNode, nextNode.getDepth(), alpha, beta).getRank();
 //			System.out.println("Alpha: " + alpha + " Beta: " + beta);
+			
+			if (rank > alpha)
+				alpha = rank;
 			
 			if (rank > bestRank) {
 				bestRank = rank;
 				bestNode = nextNode;
 			}
-			
-			if (rank > alpha)
-				alpha = rank;
-
 
 		}
 		bestNode.setRank(bestRank);
@@ -101,43 +103,31 @@ public class CaptainDeMorganAI extends CKPlayer {
 			return n;
 		}
 		
-		HashSet<Point> nextMoves = n.nextMoves(n.getState().gravityEnabled());
+		HashSet<Point> nextMoves = n.getCacheNextMoves();
 		int bestRank = Integer.MAX_VALUE;
 		Node bestNode = null;
-		ArrayList<Node> leafNodes = new ArrayList<Node>();
 		
 		for (Point p : nextMoves) {
+			Node nextNode = new Node(n.nextState(p), depth+1, p, n.getCutoff());
 			Node.incNodeCount();
-			if (depth+1 == n.getCutoff()) {
-				Node node = new Node(n.nextState(p), depth+1, p, n.getCutoff());
-				node.setRank(-node.heuristic(p));
-				leafNodes.add(node);
-				
+			int rank = max(nextNode, nextNode.getDepth(), alpha, beta).getRank();
+			
+			if (rank < beta)
+				beta = rank;
+			
+			if (rank < bestRank) {
+				bestRank = rank;
+				bestNode = nextNode;
 			}
-			else {
-				Node nextNode = new Node(n.nextState(p), depth+1, p, n.getCutoff());
-				int rank = max(nextNode, nextNode.getDepth(), alpha, beta).getRank();
-	
-				if (rank < bestRank) {
-					bestRank = rank;
-					bestNode = nextNode;
-				}
-				
-				if (rank < beta)
-					beta = rank;			
-				if (alpha >= beta)
-					break;
-			}
+			
+			if (alpha >= beta)
+				break;
 		}
 				
-		if (depth+1 == n.getCutoff()) {
-			Collections.sort(leafNodes);
-			bestNode = leafNodes.get(0);
-		}
-		else
-			bestNode.setRank(bestRank);
+		bestNode.setRank(bestRank);
 		return bestNode;
 	}
+
 
 	private Node max(Node n, int depth, int alpha, int beta) {
 		// maximize player 2
@@ -151,43 +141,29 @@ public class CaptainDeMorganAI extends CKPlayer {
 			return n;
 		}
 		
-		HashSet<Point> nextMoves = n.nextMoves(n.getState().gravityEnabled());
+		HashSet<Point> nextMoves = n.getCacheNextMoves();
 		int bestRank = Integer.MIN_VALUE;
 		Node bestNode = null;
-		ArrayList<Node> leafNodes = new ArrayList<Node>();
 		
 		for (Point p : nextMoves) {
+			Node nextNode = new Node(n.nextState(p), depth+1, p, n.getCutoff());
 			Node.incNodeCount();
-			if (depth+1 == n.getCutoff()) {
-				Node node = new Node(n.nextState(p), depth+1, p, n.getCutoff());
-				node.setRank(node.heuristic(p));
-				leafNodes.add(node);
-				
-			}
-			else {
-				Node nextNode = new Node(n.nextState(p), depth+1, p, n.getCutoff());
-				int rank = min(nextNode, nextNode.getDepth(), alpha, beta).getRank();
+			int rank = min(nextNode, nextNode.getDepth(), alpha, beta).getRank();
 			
-				if (rank > bestRank) {
-					bestRank = rank;
-					bestNode = nextNode;
-				}
-				
-				if (rank > alpha)
-					alpha = rank;
-				
-				if (alpha >= beta)
-					break;
+			if (rank > alpha)
+				alpha = rank;
+			
+			if (rank > bestRank) {
+				bestRank = rank;
+				bestNode = nextNode;
 			}
+			
+			if (alpha >= beta)
+				break;
 
 		}
 		
-		if (depth+1 == n.getCutoff()) {
-			Collections.sort(leafNodes, Collections.reverseOrder());
-			bestNode = leafNodes.get(0);
-		}
-		else
-			bestNode.setRank(bestRank);
+		bestNode.setRank(bestRank);
 		return bestNode;
 	}
 		
--- Node.java
+++ Node.java
@@ -1,10 +1,8 @@
-
 import java.awt.Point;
-import java.util.Comparator;
 import java.util.HashSet;
 import connectK.BoardModel;
 
-public class Node implements Comparable<Node>{
+public class Node {
 		private int rank;
 		private int depth;
 		private BoardModel state;
@@ -13,39 +11,31 @@ public class Node implements Comparable<Node>{
 		private int opponent;
 		private int cutoff; // maximum depth to be evaluated, inclusive; root depth = 0
 		private static int nodeCount = 0;
+		private HashSet<Point> cacheNextMoves;
 		
 		Node(BoardModel state, int depth, Point move, int cutoff) {
 			this.depth = depth;
 			this.state = state.clone();
 			this.move = move;
-			this.player = this.currentPlayer();
-			this.opponent = this.opponentPlayer();
+			player = currentPlayer();
+			opponent = opponentPlayer();
 			this.cutoff = cutoff;
+			cacheNextMoves = nextMoves(state.gravityEnabled());
 			
 		}
 		
-		@Override
-		public int compareTo(Node n) {
-			if (this.rank < n.getRank())
-				return -1;
-			else if (this.rank == n.getRank())
-				return 0;
-			else
-				return 1;
-		}
-		
 		private int opponentPlayer() {
-			if (this.player == 1)
+			if (player == 1)
 				return 2;
 			else
 				return 1;
 		}
 		
 		private int currentPlayer() {
-			Point p = this.state.getLastMove();
+			Point p = state.getLastMove();
 			if (p == null) // game just started so player 1 goes first by default
 				return 1;
-			else if (this.state.getSpace(p) == 1)
+			else if (state.getSpace(p) == 1)
 				return 2;
 			else
 				return 1;
@@ -53,24 +43,23 @@ public class Node implements Comparable<Node>{
 		}
 		
 		public void evalGame() {
-			byte winner = this.state.winner();
+			byte winner = state.winner();
 		
-			// this part works
-			if (winner == 1)
-				this.rank = Integer.MIN_VALUE + 1;
-			else if (winner == 2)
-				this.rank = Integer.MAX_VALUE - 1;
+			if (winner == opponent)
+				rank = Integer.MIN_VALUE + 1;
+			else if (winner == player)
+				rank = Integer.MAX_VALUE - 1;
 			else if (winner == 0)
-				this.rank = 0;
+				rank = 0;
 		}
 		
 		public HashSet<Point> nextMoves(boolean gravity) {
 			HashSet<Point> moves = new HashSet<Point>();
 			
 			if (gravity) {
-				for (int x = 0; x < this.state.getWidth(); x++) {
-					for (int y = 0; y < this.state.getHeight(); y++) {
-						if (this.state.getSpace(x,y) == 0) {
+				for (int x = 0; x < state.getWidth(); x++) {
+					for (int y = 0; y < state.getHeight(); y++) {
+						if (state.getSpace(x,y) == 0) {
 							moves.add(new Point(x,y));
 							break;
 						}
@@ -79,9 +68,9 @@ public class Node implements Comparable<Node>{
 			}
 			
 			else {
-				for (int x = 0; x < this.state.getWidth(); ++x) {
-					for (int y = 0; y < this.state.getHeight(); ++y) {
-						if (this.state.getSpace(x,y) == 0) {
+				for (int x = 0; x < state.getWidth(); ++x) {
+					for (int y = 0; y < state.getHeight(); ++y) {
+						if (state.getSpace(x,y) == 0) {
 							moves.add(new Point(x,y));
 						}
 					}
@@ -93,133 +82,173 @@ public class Node implements Comparable<Node>{
 		
 		public BoardModel nextState(Point move) {
 			
-			BoardModel nextState = this.state.clone();
-			return nextState.placePiece(move, (byte) this.player);
+			BoardModel nextState = state.clone();
+			return nextState.placePiece(move, (byte) player);
 
 		}
 				
 		public int heuristic(Point p) {
-//			int rank = 0;
-//			for (int x = 0; x < this.state.getWidth(); x++) {
-//				for (int y = 0; y < this.state.getHeight(); y++) {
-//					if (this.state.getSpace(x, y) == this.getPlayer()) {
-//						rank += checkEightDirections(new Point(x,y));
-//					}
-//				}
-//			}
-			return this.checkEightDirections(p);
+			int sum = 0;
+				
+			if (!state.gravityEnabled()) {
+				return checkEightDirections(p);
+			}
+			else {
+				for (Point pt : cacheNextMoves) {
+					sum += checkEightDirections(pt);
+				}
+			}
+			return sum;
 		}		
 	
 		public int checkEightDirections(Point p) {
-			// number-of-possible-wins heuristic
-			// consider passing heuristic() into the constructor as an argument (lambda in J8 an or interface in J7)
+			// Summing the player's pieces within k spaces of P
 			// By definition, a heuristic always underestimates the actual cost to a goal node.
-			// minimizer: -10 < h(n) <= 0
-			// maximizer: 0 <= h(n) < 10
-			int hRank = 0;
+			int pieceRank = 10;
+			int emptyRank = 1;
+			int hRank = -(emptyRank * 8) + pieceRank; // to account for over-counting 
+			
 
 			// top to bottom
-			for (int x = p.x; x < this.state.getWidth(); x++) {
-				if (this.state.getSpace(x, p.y) == this.opponent)
+//			System.out.println("top to bottom");
+			for (int x = p.x, k = 0; x < state.getWidth(); x++, k++) {
+				if (state.getSpace(x, p.y) == opponent || k == state.getkLength())
 					break;
-				else if (this.state.getSpace(x, p.y) == 0)
-					hRank += 1;
-				else if (this.state.getSpace(x, p.y) == this.player)
-					hRank += 2;
+				else if (state.getSpace(x, p.y) == 0)
+					hRank += emptyRank;
+				else if (state.getSpace(x, p.y) == player)
+					hRank += pieceRank;
+//				System.out.print("(" + x + "," + p.y + ")");
 			}
-	
+//			System.out.println();
+			
 			// bottom to top
-			for (int x = p.x; x >= 0; x--) {
-				if (this.state.getSpace(x, p.y) == this.opponent)
+//			System.out.println("bottom to top");
+			for (int x = p.x, k = 0; x >= 0; x--, k++) {
+				if (state.getSpace(x, p.y) == opponent || k == state.getkLength())
 					break;
-				else if (this.state.getSpace(x, p.y) == 0)
-					hRank += 1;
-				else if (this.state.getSpace(x, p.y) == this.player)
-					hRank += 2;
+				else if (state.getSpace(x, p.y) == 0)
+					hRank += emptyRank;
+				else if (state.getSpace(x, p.y) == player)
+					hRank += pieceRank;
+//				System.out.print("(" + x + "," + p.y + ")");
 			}
+//			System.out.println();
 			
 			// left to right
-			for (int y = p.y; y < this.state.getHeight(); y++) {
-				if (this.state.getSpace(p.x, y) == this.opponent)
+//			System.out.println("left to right");
+			for (int y = p.y, k = 0; y < state.getHeight(); y++, k++) {
+				if (state.getSpace(p.x, y) == opponent || k == state.getkLength())
 					break;
-				else if (this.state.getSpace(p.x, y) == 0)
-					hRank += 1;
-				else if (this.state.getSpace(p.x, y) == this.player)
-					hRank += 2;
+				else if (state.getSpace(p.x, y) == 0)
+					hRank += emptyRank;
+				else if (state.getSpace(p.x, y) == player)
+					hRank += pieceRank;
+//				System.out.print("(" + p.x + "," + y + ")");
 			}
+//			System.out.println();
 			
 			// right to left
-			for (int y = p.y; y >= 0; y--) {
-				if (this.state.getSpace(p.x, y) == this.opponent)
+//			System.out.println("right to left");
+			for (int y = p.y, k = 0; y >= 0; y--, k++) {
+				if (state.getSpace(p.x, y) == opponent || k == state.getkLength())
 					break;
-				else if (this.state.getSpace(p.x, y) == 0)
-					hRank += 1;
-				else if (this.state.getSpace(p.x, y) == this.player)
-					hRank += 2;
+				else if (state.getSpace(p.x, y) == 0)
+					hRank += emptyRank;
+				else if (state.getSpace(p.x, y) == player)
+					hRank += pieceRank;
+//				System.out.print("(" + p.x + "," + y + ")");
 			}
+//			System.out.println();
 			
 			// upper-right
+//			System.out.println("upper right");
 			try {
 				
 				int y = p.y;
-				for (int x = p.x; x >= 0; x--) {
-					if (this.state.getSpace(x, y) == this.opponent)
+				for (int x = p.x, k = 0; x >= 0; x--, k++) {
+					if (state.getSpace(x, y) == opponent || k == state.getkLength())
 						break;
-					else if (this.state.getSpace(x, y) == 0)
-						hRank += 1;
-					else if (this.state.getSpace(x, y) == this.player)
-						hRank += 2;
+					else if (state.getSpace(x, y) == 0)
+						hRank += emptyRank;
+					else if (state.getSpace(x, y) == player)
+						hRank += pieceRank;
+//					System.out.print("(" + x + "," + y + ")");
 					y++;
+					
 
 				}
 			} catch (IndexOutOfBoundsException e) {}
+//			System.out.println();
 			
 			// lower-left
+//			System.out.println("lower left");
 			try {
 				
 				int y = p.y;
-				for (int x = p.x; x < this.state.getWidth(); x++) {
-					if (this.state.getSpace(x, y) == this.opponent)
+				for (int x = p.x, k = 0; x < state.getWidth(); x++, k++) {
+					if (state.getSpace(x, y) == opponent || k == state.getkLength())
 						break;
-					else if (this.state.getSpace(x ,y) == 0)
-						hRank += 1;
-					else if (this.state.getSpace(x, y) == this.player)
-						hRank += 2;
+					else if (state.getSpace(x ,y) == 0)
+						hRank += emptyRank;
+					else if (state.getSpace(x, y) == player)
+						hRank += pieceRank;
+//					System.out.print("(" + x + "," + y + ")");
 					y--;
 				}
 			} catch (IndexOutOfBoundsException e) {}
+//			System.out.println();
 			
 			// upper-left
+//			System.out.println("upper left");
 			try {
 				int y = p.y;
-				for (int x = p.x; x >= 0; x--) {
-					if (this.state.getSpace(x, y) == this.opponent)
+				for (int x = p.x, k = 0; x >= 0; x--, k++) {
+					if (state.getSpace(x, y) == opponent || k == state.getkLength())
 						break;
-					else if (this.state.getSpace(x, y) == 0)
-						hRank += 1;
-					else if (this.state.getSpace(x, y) == this.player)
-						hRank += 2;
+					else if (state.getSpace(x, y) == 0)
+						hRank += emptyRank;
+					else if (state.getSpace(x, y) == player)
+						hRank += pieceRank;
+//					System.out.print("(" + x + "," + y + ")");
 					y--;
 				}
 			} catch (IndexOutOfBoundsException e) {}
-			
+//			System.out.println();
 		
 			// lower-right
+//			System.out.println("lower right");
 			try {	
 				
 				int y = p.y;
-				for (int x = p.x; x < this.state.getWidth(); x++) {
-					if (this.state.getSpace(x, y) == this.opponent)
+				for (int x = p.x, k = 0; x < state.getWidth(); x++, k++) {
+					if (this.state.getSpace(x, y) == opponent || k == state.getkLength())
 						break;
-					else if (this.state.getSpace(x, y) == 0)
-						hRank += 1;
-					else if (this.state.getSpace(x, y) == this.player)
-						hRank += 2;
+					else if (state.getSpace(x, y) == 0)
+						hRank += emptyRank;
+					else if (state.getSpace(x, y) == player)
+						hRank += pieceRank;
+//					System.out.print("(" + x + "," + y + ")");
 					y++;
 				}
 			} catch (IndexOutOfBoundsException e) {}
+//			System.out.println();
 					
 			return hRank;
+		}
+		
+		public void printCoord() {
+			for (int r = 0; r < state.getWidth(); r++) {
+				for (int c = 0 ; c < state.getHeight(); c++) {
+					System.out.print("(" + r + "," + c + ")");
+				}
+				System.out.println();
+			}
+			System.out.println("top to bottom ->");
+			System.out.println("<- bottom to top");
+			System.out.println("left to right (UP)");
+			System.out.println("right to left (DOWN)");
+			
 		}
 		
 		public static int getNodeCount() {
@@ -274,29 +303,34 @@ public class Node implements Comparable<Node>{
 			this.rank = r;
 		}
 
+		public HashSet<Point> getCacheNextMoves() {
+			return cacheNextMoves;
+		}
 
-
-
-
-
+		public void setCacheNextMoves(HashSet<Point> cacheNextMoves) {
+			this.cacheNextMoves = cacheNextMoves;
+		}
 		
+		
+
 //		public static void main(String[] args) {
-//			BoardModel board = new BoardModel(3, 3, 3, false);
+//			BoardModel board = new BoardModel(5, 5, 3, false);
 //			board = board.placePiece(new Point(1,1), (byte) 1);
-//			board = board.placePiece(new Point(0,1), (byte) 2);
-////			board = board.placePiece(new Point(1,0), (byte) 2);
+//			board = board.placePiece(new Point(0,2), (byte) 2);
+//			board = board.placePiece(new Point(0,3), (byte) 2);
 ////			board = board.placePiece(new Point(2,0), (byte) 2);
-////			board = board.placePiece(new Point(2,1), (byte) 1);
+//			board = board.placePiece(new Point(4,4), (byte) 1);
 ////			board = board.placePiece(new Point(2,2), (byte) 1);
 //
 ////			board.placePiece(new Point(2,2), (byte) 1);
 //			
 //			Node n = new Node(board, 0, null, 3);
-//			
+//			n.printCoord();
 //			System.out.println("Current player:" + n.getPlayer());
 //			System.out.println(n.getState().toString());
-//			System.out.println(n.heuristic());
-//			System.out.println(n.getState().toString());
+//			System.out.println(n.heuristic(new Point(0,1)));
+//			System.out.println("After move is applied to where the heuristic is calculating");
+//			System.out.println(n.getState().placePiece(new Point(0,1), (byte) 2).toString());
 //			
 //		}
 	}
